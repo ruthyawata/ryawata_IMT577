987e8ab745fec3f13877ba649cd81302fd6107d9
ruthyawata <ryawata@uw.edu>
2022-05-16 15:17:30 -0700

    added file for creating views in IT session and DW

---
+++ AssignmentCode/IMT577-views.sql
@@ -0,0 +1,17 @@
+/***
+View 1 - store sales vs target
+view 2 - bonus based on product sales
+view 3 - sales by day of week for products
+view 4 - state-wide sales performance
+
+1. Give an overall assessment of stores number 5 and 8’s sales.
+
+How are they performing compared to target? Will they meet their 2021 target?
+Should either store be closed? Why or why not?
+What should be done in the next year to maximize store profits?
+2. Recommend separate 2020 and 2021 bonus amounts for each store if the total bonus pool for 2020 is $500,000 and the total bonus pool for 2021 is $400,000. Base your recommendation on how well the stores are selling Product Types of Men’s Casual and Women’s Casual.
+
+3. Assess product sales by day of the week at stores 5 and 8. What can we learn about sales trends?
+
+4. Compare the performance of all stores located in states that have more than one store to all stores that are the only store in the state. What can we learn about having more than one store in a state?
+***/

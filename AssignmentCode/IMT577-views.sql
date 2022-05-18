e8f337c1f3d6b474c81b0d84b39d328ece85508c
ruthyawata <ryawata@uw.edu>
2022-05-18 15:22:19 -0700

    attempt at first view to answer q1

--- AssignmentCode/IMT577-views.sql
+++ AssignmentCode/IMT577-views.sql
@@ -15,3 +15,32 @@ What should be done in the next year to maximize store profits?
 
 4. Compare the performance of all stores located in states that have more than one store to all stores that are the only store in the state. What can we learn about having more than one store in a state?
 ***/
+CREATE VIEW ProductSales
+AS
+    SELECT DISTINCT
+    Dim_Store.StoreNumber
+    ,Dim_Date.Year
+    ,Fact_SRCSalesTarget.SalesTargetAmount
+    ,SUM(Fact_SalesActual.SaleTotalProfit) AS Amount
+    FROM Dim_Store
+    INNER JOIN Fact_SRCSalesTarget ON
+    Fact_SRCSalesTarget.DimStoreID = Dim_Store.DimStoreID
+    INNER JOIN Fact_SalesActual on
+    Fact_SalesActual.DimStoreID = Dim_Store.DimStoreID
+    INNER JOIN Dim_Date ON
+    Dim_Date.Date_Pkey = Fact_SalesActual.DimSaleDateID
+    WHERE Dim_Store.StoreNumber = 5
+    GROUP BY Dim_Date.Year, Dim_Store.StoreNumber, Fact_SRCSalesTarget.SalesTargetAmount
+    ORDER BY Dim_Store.StoreNumber, Dim_Date.Year
+-- group by aggregate for daily and then it's the average of those days of the week
+select sum(saleamount) 
+from Fact_SalesActual 
+WHERE DimStoreID = 1 AND DimSaleDateID LIKE '2013%'
+select SalesTargetAmount from Fact_SRCSalesTarget WHERE DimStoreID = 1 GROUP BY SalesTargetAmount
+
+SELECT SUM(Fact_SalesActual.SaleTotalProfit) AS Profit
+FROM Fact_SalesActual 
+INNER JOIN 
+WHERE DimStoreID = 1 
+AND DimSaleDateID LIKE '2013%'
+sele

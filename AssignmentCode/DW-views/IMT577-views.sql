24f09ee35b703d3b62682da999b917fdd725f5cf
ruthyawata <ryawata@uw.edu>
2022-05-29 21:34:40 -0700

    added views for daily sales and targets, bonus

--- AssignmentCode/DW-views/IMT577-views.sql
+++ AssignmentCode/DW-views/IMT577-views.sql
@@ -166,7 +166,7 @@ CREATE OR REPLACE SECURE VIEW StoresbyState_Target
     ORDER BY Dim_Store.StoreNumber, State, ZipCode, Dim_Date.Year, Target
 
 -- View 11 Product sales by day
- CREATE OR REPLACE SECURE VIEW ProductSalesType_DayofWeek
+CREATE OR REPLACE SECURE VIEW ProductSalesType_DayofWeek
     AS
     SELECT DISTINCT
             Dim_Store.StoreNumber
@@ -183,3 +183,93 @@ CREATE OR REPLACE SECURE VIEW StoresbyState_Target
         WHERE Dim_Store.StoreNumber = 5 OR Dim_Store.StoreNumber = 8
         GROUP BY Dim_Store.StoreNumber, Dim_Product.ProductType, Dim_Date.Day_Abbrev
         ORDER BY ProductSales DESC
+
+-- View 11 Product category by day
+CREATE OR REPLACE SECURE VIEW ProductCategory_DayofWeek
+    AS
+    SELECT DISTINCT
+            Dim_Store.StoreNumber
+            ,Dim_Product.ProductCategory
+            ,Dim_Date.Day_Abbrev
+            ,SUM(Fact_SalesActual.SaleAmount) AS ProductSales
+        FROM Fact_SalesActual
+        INNER JOIN Dim_Store ON
+        Fact_SalesActual.DimStoreID = Dim_Store.DimStoreID
+        INNER JOIN Dim_Date ON
+        Fact_SalesActual.DimSaleDateID = Dim_Date.Date_Pkey
+        INNER JOIN Dim_Product ON
+        Fact_SalesActual.DimProductID = Dim_Product.DimProductID
+        WHERE Dim_Store.StoreNumber = 5 OR Dim_Store.StoreNumber = 8
+        GROUP BY Dim_Store.StoreNumber, Dim_Product.ProductCategory, Dim_Date.Day_Abbrev
+        ORDER BY ProductSales DESC
+
+-- View 12 Daily sales amount
+CREATE OR REPLACE SECURE VIEW Sales_Daily
+    AS
+    SELECT DISTINCT
+        Dim_Store.StoreNumber
+        ,Dim_Date.Date_Pkey
+        ,Dim_Date.Year
+        ,SUM(Fact_SalesActual.SaleAmount) AS SalesActual
+    FROM Fact_SalesActual
+    INNER JOIN Dim_Date
+    ON Fact_SalesActual.DimSaleDateID = Dim_Date.Date_Pkey
+    INNER JOIN Dim_Store ON
+    Fact_SalesActual.DimStoreID = Dim_Store.DimStoreID
+    WHERE Dim_Store.StoreNumber = 5 OR Dim_Store.StoreNumber = 8
+    GROUP BY Dim_Store.StoreNumber, Dim_Date.Date_Pkey, Dim_Date.Year
+    ORDER BY Dim_Store.StoreNumber, Dim_Date.Date_Pkey
+
+-- View 13 Daily target amount
+CREATE OR REPLACE SECURE VIEW Target_Daily
+    AS
+    SELECT DISTINCT
+        Dim_Store.StoreNumber
+        ,Dim_Date.Date_Pkey
+        ,Dim_Date.Year
+        ,ROUND(Fact_SRCSalesTarget.SalesTargetAmount / 365, 2) AS DailyTarget
+    FROM Fact_SRCSalesTarget
+    INNER JOIN Dim_Date
+    ON Fact_SRCSalesTarget.DimTargetDateID = Dim_Date.Date_PKEY
+    INNER JOIN DIM_STORE
+    ON Fact_SRCSalesTarget.DimStoreID = Dim_Store.DimStoreID
+    WHERE Dim_Store.StoreNumber = 5 OR Dim_Store.StoreNumber = 8
+    ORDER BY Dim_Store.StoreNumber, Dim_Date.Date_Pkey
+
+-- View 13 Bonus recommendation
+CREATE OR REPLACE SECURE VIEW Bonus_Recommendation
+    AS
+    SELECT DISTINCT
+        Year
+        ,StoreNumber
+        ,ProductType
+        ,Sale
+        ,Profit
+        ,SUM(Sale) OVER (PARTITION BY Year) AS Total_Sale
+        ,(Sale/Total_Sale) AS Amount
+    ,ROUND(
+        CASE 
+            WHEN Year = 2013 THEN 500000*(Sale/Total_Sale)
+            WHEN Year = 2014 THEN 400000*(Sale/Total_Sale)
+        END,2) AS Bonus_Amount
+    FROM
+    (
+        SELECT DISTINCT 
+            Dim_Date.Year
+            ,Dim_Store.StoreNumber
+            ,Dim_Product.ProductType
+            ,SUM(Fact_SalesActual.SaleAmount) AS Sale
+            ,SUM(FAct_SalesActual.SaleTotalProfit) AS Profit
+        FROM Fact_SalesActual 
+        INNER JOIN Dim_Store 
+        ON Fact_SalesActual.DimStoreID = Dim_Store.DimStoreID
+        INNER JOIN Dim_Product 
+        ON Fact_SalesActual.DimProductID = Dim_Product.DimProductID
+        INNER JOIN Dim_Date 
+        ON Fact_SalesActual.DIMSALEDATEID = Dim_Date.Date_Pkey
+        WHERE Dim_Product.ProductType
+        LIKE '%Casual' 
+        AND Dim_Store.StoreNumber IN (5,8)
+        GROUP BY Dim_Date.Year, Dim_Store.StoreNumber, Dim_Product.ProductType
+    )
+    ORDER BY Year, StoreNumber
